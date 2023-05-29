1e3c1577447bbfa74a17df432d7e30353096e2db
Abeera Bari <39463752+abeera17@users.noreply.github.com>
2023-05-29 20:57:26 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        gray: "#F5F5F5",
+        orange: "#FE5431",
+        parrot: "#6EA70E",
+      },
+      fontFamily: {
+        Baloo: "'Baloo Paaji 2', cursive",
+        kaushan: "'Kaushan Script', cursive",
+        monserrat: "Montserrat', sans-serif",
+        popins: "'Poppins', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
